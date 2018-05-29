90a1da9d2e55e02230238e4ffa83d2b01d18f228
Unknown <vitor.fa.morais@gmail.com>
2018-05-30 00:11:34 +0100

    kitchen sink

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,17 @@
 // Dom7
-var $$ = Dom7;
+var $ = Dom7;
 
-// Framework7 App main instance
-var app  = new Framework7({
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
-  // App root data
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  theme: theme,
   data: function () {
     return {
       user: {
@@ -16,38 +20,13 @@ var app  = new Framework7({
       },
     };
   },
-  // App root methods
   methods: {
     helloWorld: function () {
       app.dialog.alert('Hello World!');
     },
   },
-  // App routes
   routes: routes,
-  // Enable panel left visibility breakpoint
-  panel: {
-    leftBreakpoint: 960,
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
   },
-});
-
-// Init/Create left panel view
-var mainView = app.views.create('.view-left', {
-  url: '/'
-});
-
-// Init/Create main view
-var mainView = app.views.create('.view-main', {
-  url: '/'
-});
-
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
